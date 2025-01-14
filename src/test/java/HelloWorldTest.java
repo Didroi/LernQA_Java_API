d401212f13f9ff2c74b16ea7e127e8c5fcfd8240
Dmitrii Kiselev <didroi@yandex.ru>
2025-01-14 20:41:58 +0100

    greetings

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -1,9 +1,20 @@
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 
 public class HelloWorldTest {
 
     @Test
-    public void testHelloWorld() {
-        System.out.println("Hello World!");
+    public void greetings() {
+        System.out.println("Hello from Dmitrii");
     }
+
+//    @Test
+//    public void firstApiTest() {
+//        Response response = RestAssured
+//                .get("https://playground.learnqa.ru/api/hello")
+//                .andReturn();
+//        response.prettyPrint();
+//    }
 }
